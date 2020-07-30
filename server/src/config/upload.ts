d426fbf19ddb24f0f7f5673a07a9e496d62746e5
Túlio Navarro <tnavarrodev@gmail.com>
2020-07-30 11:10:03 -0300

    add s3 storage

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -1,19 +1,49 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 import path from 'path';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {
+      storage: StorageEngine;
+    };
+    s3: {
+      bucket: string;
+      base_url: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVE,
+
   tmpFolder,
   uploadFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    s3: {
+      bucket: 'gobarber',
+      base_url: 'https://gobarber.s3.Region.amazonaws.com/',
     },
-  }),
-};
+  },
+} as IUploadConfig;
